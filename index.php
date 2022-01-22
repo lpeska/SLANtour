07b2af314f9c10efc97fb92dae3f841622b17558
lpesk <lpesk@DESKTOP-JTI0EPA>
2022-01-22 11:24:33 +0100

    init

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment($loader, [
+    'cache' => 'cache',
+]);
+
+echo $twig->render('index.html', ['name' => 'Fabien']);
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
