a57af5d0424aa24faa99bb03b0202abbabc41ef2
Honza Peska <peska.honza@seznam.cz>
2022-01-23 19:14:59 +0100

    disable twig cache and fix stylesheets

--- index.php
+++ index.php
@@ -2,9 +2,7 @@
 require_once 'vendor/autoload.php';
 
 $loader = new \Twig\Loader\FilesystemLoader('templates');
-$twig = new \Twig\Environment($loader, [
-    'cache' => 'cache',
-]);
+$twig = new \Twig\Environment($loader);
 
 echo $twig->render('index.html.twig', ['name' => 'Fabien']);
 
