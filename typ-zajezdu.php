f90efc4771b1b5fd92c73827caacacd9880c5273
Honza Peska <peska.honza@seznam.cz>
2022-04-15 02:10:46 +0200

    add types grid to list of types

--- typ-zajezdu.php
+++ typ-zajezdu.php
@@ -13,6 +13,10 @@ echo $twig->render('typ-zajezdu.html.twig', [
         new Tour('Hotel Esprit***, Špindlerův Mlýn', 1470, 29, 2070, 4, 'Polopenze', 'Krkonoše', '/img/lazne.png'),
         new Tour('Víkend v Budapešti - vlakem', 3590, 0, 3590, 4, 'bez stravy', 'Maďarsko', '/img/dovolena.png'),
         new Tour('Jordánsko s pobytem u Rudého moře', 29990, 25, 39986, 7, 'All-inclusive', 'Jordánsko', '/img/poznavaci.png'),
+        new Tour('Villa Dino, Mariánské Lázně', 4790, 0, 4790, 4, 'Polopenze', 'Mariánské Lázně', '/img/lazne.png'),
+        new Tour('Hotel Esprit***, Špindlerův Mlýn', 1470, 29, 2070, 4, 'Polopenze', 'Krkonoše', '/img/lazne.png'),
+        new Tour('Víkend v Budapešti - vlakem', 3590, 0, 3590, 4, 'bez stravy', 'Maďarsko', '/img/dovolena.png'),
+        new Tour('Jordánsko s pobytem u Rudého moře', 29990, 25, 39986, 7, 'All-inclusive', 'Jordánsko', '/img/poznavaci.png'),
         new Tour('Villa Dino, Mariánské Lázně', 4790, 0, 4790, 4, 'Polopenze', 'Mariánské Lázně', '/img/lazne.png')
     )
 ]);
--- typy-zajezdu.php
+++ typy-zajezdu.php
@@ -11,14 +11,13 @@ echo $twig->render('typy-zajezdu.html.twig', [
     'typesOfTours' => array(
         new TourType('Poznávací', 139, 9900, 'img/poznavaci.png'),
         new TourType('Eurovíkendy', 62, 15900, 'img/eurovikendy.png'),
-        new TourType('Dovolená', 140, 7900, 'img/dovolena.png'),
+        new TourType('Dovolená u moře', 140, 7900, 'img/dovolena.png'),
         new TourType('Lázně & Wellness', 79, 3900, 'img/lazne.png'),
         new TourType('Sport', 32, 7900, 'img/sport.png'),
-        new TourType('Poznávací', 139, 9900, 'img/poznavaci.png'),
-        new TourType('Eurovíkendy', 62, 15900, 'img/eurovikendy.png'),
-        new TourType('Dovolená', 140, 7900, 'img/dovolena.png'),
-        new TourType('Lázně & Wellness', 79, 3900, 'img/lazne.png'),
-        new TourType('Sport', 32, 7900, 'img/sport.png'),
+        new TourType('Tuzemské pobyty', 139, 9900, 'img/poznavaci.png'),
+        new TourType('Fly and Drive', 140, 7900, 'img/dovolena.png'),
+        new TourType('Exotické zájezdy', 79, 3900, 'img/lazne.png'),
+        new TourType('Jednodenní zájezdy', 32, 7900, 'img/sport.png'),
     )
     ]);
 
