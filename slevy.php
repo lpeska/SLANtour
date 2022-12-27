610ee5616c66cc480e54f3e0975af4dcc4d36a0d
Honza Peska <peska.honza@seznam.cz>
2022-12-27 16:23:09 +0100

    updates to menu, contacts and about

---
+++ slevy.php
@@ -0,0 +1,24 @@
+<?php
+require_once 'vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment($loader, [
+    'debug' => true,
+]);
+$twig->addExtension(new \Twig\Extension\DebugExtension());
+
+echo $twig->render('slevy.html.twig', [
+    'breadcrumbs' => array(
+        new Breadcrumb('Slevy', '/slevy.php')
+    )
+]);
+
+class Breadcrumb {
+    public string $label;
+    public string $link;
+
+    public function __construct(string $label, string $link) {
+        $this->label = $label;
+        $this->link = $link;
+    }
+}
