6c1404b26d9073aaba6c1d1e7cce746aaa5dfdee
peskajan <peska.honza@seznam.cz>
2023-09-28 19:10:47 +0200

    sync classes, load types menu everywhere - need to check

--- zajezd.php
+++ zajezd.php
@@ -1,5 +1,7 @@
 <?php
 require_once 'vendor/autoload.php';
+require_once "./classes/loadDataTwig.inc.php"; //funkce na nacitani zajezdu, menu a classes
+$tourTypes = getAllTourTypes();
 
 $loader = new \Twig\Loader\FilesystemLoader('templates');
 $twig = new \Twig\Environment($loader, [
@@ -8,6 +10,7 @@ $twig = new \Twig\Environment($loader, [
 $twig->addExtension(new \Twig\Extension\DebugExtension());
 
 echo $twig->render('zajezd.html.twig', [
+    'typesOfTours' => $tourTypes,
     'name' => 'HOTEL BUSIGNANI***, RIMINI RIVABELLA',
     'priceFrom' => 15900,
     'priceDiscount' => 29,
