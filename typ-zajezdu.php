53be9e0a6931f0dafaea0a8064269c1f7ed932ef
peskajan <peska.honza@seznam.cz>
2023-10-06 16:19:14 +0200

    Fix breadcrumbs

--- typ-zajezdu.php
+++ typ-zajezdu.php
@@ -1,18 +1,21 @@
 <?php
 require_once 'vendor/autoload.php';
-require_once "./core/load_core.inc.php"; 
+require_once "./core/load_core.inc.php";
 require_once "./classes/loadDataTwig.inc.php"; //funkce na nacitani zajezdu, menu a classes
 
 $url = "$_SERVER[REQUEST_URI]";
 $values = parse_url($url);
-$path = explode('/',$values['path']);
+$path = explode('/', $values['path']);
 
 $typeName = $path[count($path) - 1];
 
 $type = getTourType($typeName);
 $tourTypes = getAllTourTypes();
 
-$breadCrumbs = array(new Breadcrumb($type->name, $type->url));
+$breadCrumbs = array(
+    new Breadcrumb('Typy zájezdů', '/typy-zajezdu.php'),
+    new Breadcrumb($type->name, $type->url)
+);
 
 $discountTours = getDiscountTours($typeName);
 
