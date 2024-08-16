70089281bc1ab48af2c29b84046a7800c0393ec0
lpesk <lpesk@DESKTOP-JTI0EPA>
2024-08-16 11:25:59 +0200

    Zprovozneni map

--- html-template/js/main.js
+++ html-template/js/main.js
@@ -489,23 +489,29 @@
 		zoomOffset: -1,
 		accessToken: "pk.eyJ1IjoibHBlc2thIiwiYSI6ImNrYmx5dGh4cjA3MHMycW1pdHp4Y2ZheGoifQ.e-0fQLJYoUUxsM0X6Z-gxQ"
 	};
+        
+        window.addEventListener("load", function(){
+            var slanyX = 50.230446218120264;
+            var slanyY = 14.082620927422633;
+            var slanyText = 'Cestovní kancelář Slantour';
 
-	var slanyX = 50.230446218120264;
-	var slanyY = 14.082620927422633;
-	var slanyText = 'Cestovní kancelář Slantour';
-	
-	var roudniceY = 50.423300;
-	var roudniceX = 14.255568;
-	var roudniceText = 'Prodejna Slantour - Roudnice nad Labem';
+            var roudniceY = 50.423300;
+            var roudniceX = 14.255568;
+            var roudniceText = 'Prodejna Slantour - Roudnice nad Labem';  
 
-	var mymap = L.map("map_contact").setView([slanyX, slanyY], 15);
-	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibHBlc2thIiwiYSI6ImNrYmx5dGh4cjA3MHMycW1pdHp4Y2ZheGoifQ.e-0fQLJYoUUxsM0X6Z-gxQ',
-		mapOptions).addTo(mymap);
+            
+            var mymap = L.map("map_contact").setView([slanyX, slanyY], 15);
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+              maxZoom: 19,
+              attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(mymap);                  
 
-	var slanyMarker = L.marker([slanyX,slanyY]).addTo(mymap);
-	slanyMarker.bindPopup(slanyText);
+            var slanyMarker = L.marker([slanyX,slanyY]).addTo(mymap);
+            slanyMarker.bindPopup(slanyText);
+            var roudniceMarker = L.marker([roudniceY,roudniceX]).addTo(mymap);
+            roudniceMarker.bindPopup(roudniceText);
+        });
 
-	var roudniceMarker = L.marker([roudniceY,roudniceX]).addTo(mymap);
-	roudniceMarker.bindPopup(roudniceText);
+        
 
 })(window.jQuery); 
