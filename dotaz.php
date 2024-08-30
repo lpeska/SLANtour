17b3992e44b3c44f6eb325d38d5b1a12fbe9e431
peskajan <peska.honza@seznam.cz>
2024-08-30 18:06:24 +0200

    add captcha

--- dotaz.php
+++ dotaz.php
@@ -33,78 +33,35 @@ require_once "./classes/zajezd_ubytovani.inc.php"; //seznam serialu
 
 require_once "./classes/rezervace_dotaz.inc.php"; //operace s daty klienta
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $secretKey = '6Len0DIqAAAAALXC3nx5Y3UyzjgSAOZFi1j4z2Bh'; // Replace with your Secret Key
+  $recaptchaToken = $_POST['recaptchaToken'];
+  $userIP = $_SERVER['REMOTE_ADDR'];
 
+  $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secretKey&response=$recaptchaToken&remoteip=$userIP");
+  $responseKeys = json_decode($response, true);
 
-        // if($_GET["lev4"] !=""){//mame velmi pravdepodobne stary serial+zajezd
-		             
-     
-        //   $adresa = "/zajezdy/dotaz/".$_GET["lev3"]."/".$_GET["lev4"];             
-        //     header ('HTTP/1.1 301 Moved Permanently');
-        //     header ("Location: http://".$_SERVER['HTTP_HOST'].$adresa);
-        //     exit;
-            
-        // }else if($_GET["lev3"] !=""){ //mame velmi pravdepodobne stary serial	
-            
-        //    $adresa = "/zajezdy/dotaz/".$_GET["lev3"];             
-        //     header ('HTTP/1.1 301 Moved Permanently');
-        //     header ("Location: http://".$_SERVER['HTTP_HOST'].$adresa);
-        //     exit;     
-            	                    
-        // }else if($_GET["lev1"] !=""){
-        //     if($_GET["lev2"] =="" and $_POST["id_zajezd"]>0){
-        //         $_GET["lev2"] =  $_POST["id_zajezd"];        
-        //     }            
-        //     if($_GET["lev2"] !=""){
-        //         $serial = new Serial_with_zajezd($_GET["lev1"],$_GET["lev2"]);
-        //         $zobrazit_zajezd = true;
-        //         $termin_od = $serial->get_termin_od();
-        //         $termin_do = $serial->get_termin_do();
-        //         $id_serial = $serial->get_id_serial();
-        //         $zeme_text = $serial->get_nazev_zeme_web();
-        //         $_GET["id_serial"]=$id_serial;
-        //         $_GET["id_zajezd"]=$_GET["lev2"];
-        //     }else{
-        //         $serial = new Serial($_GET["lev1"]);
-        //         $zobrazit_zajezd = false;
-        //         $id_serial = $serial->get_id();
-        //         $zeme_text = $serial->get_nazev_zeme_web();
-        //         $_GET["id_serial"]=$id_serial;
-
-        //         $serial->create_zajezdy();
-        //         if ($serial->get_zajezdy()->get_next_radek()) {//mame jeden zajezd
-        //             $id_zajezd = $serial->get_zajezdy()->get_id_zajezd();
-        //         }
-        //         if ($serial->get_zajezdy()->get_next_radek()) {//mame druhy zajezd, spatne
-        //             $no_reload_with_zajezd = true;
-        //         } else {
-        //             $no_reload_with_zajezd = 0;
-        //         }
-
-        //         if (!$no_reload_with_zajezd and $id_zajezd != "") { //mame prave jeden zajezd
-        //             $_GET["lev2"] = $id_zajezd;
-        //             $_GET["id_zajezd"]=$_GET["lev2"];
-        //             $serial = new Serial_with_zajezd($_GET["lev1"], $_GET["lev2"]);
-        //             $zobrazit_zajezd = true;
-        //         }                
-                
-                
-        //     }    
-
-        // }
-
+  if (intval($responseKeys["success"]) !== 1 || $responseKeys["score"] < 0.5) {
+      echo print_r($responseKeys);
+      echo print_r($_POST);
+      echo 'Failed CAPTCHA verification. Please try again.';
+  } else {
+    // CAPTCHA verified successfully
+    $dotaz = new Rezervace_dotaz("odeslat",$_POST["serialId"],$_POST["zajezdId"],$_POST["id_klient"],$_POST["message"],
+      $_POST["fname"],$_POST["lname"],$_POST["email"],$_POST["phone"],$_POST["novinky"]);	
+    
+    //vytvorime adresu dalsi stranku - automaticky nactenou pres http location							
+    if( !$dotaz->get_error_message() ){
+      // $adress = "/dekujeme.php?id_serial=".$_POST["id_serial"]."&typ=dotaz";
+      // $_SESSION["hlaska"]=$_SESSION["hlaska"].$dotaz->get_ok_message();
+      echo "OK";
+    }else{			
+      // $_SESSION["hlaska"]=$_SESSION["hlaska"].$dotaz->get_error_message();
+      echo "ERR";
+    }
+  }
+}
 
 				
-	$dotaz = new Rezervace_dotaz("odeslat",$_POST["serialId"],$_POST["zajezdId"],$_POST["id_klient"],$_POST["message"],
-		$_POST["fname"],$_POST["lname"],$_POST["email"],$_POST["phone"],$_POST["novinky"]);	
-
-	//vytvorime adresu dalsi stranku - automaticky nactenou pres http location							
-	if( !$dotaz->get_error_message() ){
-		// $adress = "/dekujeme.php?id_serial=".$_POST["id_serial"]."&typ=dotaz";
-		// $_SESSION["hlaska"]=$_SESSION["hlaska"].$dotaz->get_ok_message();
-    echo "OK";
-	}else{			
-		// $_SESSION["hlaska"]=$_SESSION["hlaska"].$dotaz->get_error_message();
-    echo "ERR";
-  }
         
        
--- html-template/js/main.js
+++ html-template/js/main.js
@@ -460,63 +460,60 @@
 	// registration button clicked
 	document.getElementById('registrationForm').addEventListener('submit', function (event) {
 		event.preventDefault(); // Prevent the default form submission
-
-		// const orderForm = $('#orderForm')[0];
-		// const valid = orderForm.reportValidity();
-		// if (valid) {
-		// 	const datePicker = $('#orderDate')[0];
-		// 	const dateId = datePicker[datePicker.value].dataset.dateid;
-		// 	window.location.href = "https://slantour.cz/objednavka-proces/index.php?page=zobrazit&src_web=slantour.cz&id_zajezd=" + dateId;
-		// }
-		
 		const formData = new FormData(this);
+		grecaptcha.ready(function () {
+			grecaptcha.execute('6Len0DIqAAAAAJUiTV-ox-TLQ0pHRxqWTVr7WHaw', { action: 'submit' }).then(function (token) {
+				formData.set('recaptchaToken', token);
+				// Add your logic to submit to your backend server here.
+				
+				const datePicker = $('#registrationDate')[0];
+				const dateId = datePicker[datePicker.value].dataset.dateid;
+				formData.set('zajezdId', dateId);
 
-		const datePicker = $('#registrationDate')[0];
-		const dateId = datePicker[datePicker.value].dataset.dateid;
-		formData.set('zajezdId', dateId);
+				const numberOfInterests = document.getElementById('reg1').getAttribute('data-interests');
+				var interestsString = '';
+				for (let index = 1; index <= numberOfInterests; index++) {
+					var interest = formData.get('reg' + index);
+					if (interest) {
+						if (interestsString) {
+							interestsString = interestsString + ", " + interest;
+						} else {
+							interestsString = interest;
+						}
+					}
 
-		const numberOfInterests = document.getElementById('reg1').getAttribute('data-interests');
-		var interestsString = '';
-		for (let index = 1; index <= numberOfInterests; index++) {
-			var interest = formData.get('reg' + index);
-			if (interest){
+				}
 				if (interestsString) {
-					interestsString = interestsString + ", " + interest;
-				} else {
-					interestsString = interest;
+					var dotazString = 'Mám předběžný zájem o:' + interestsString + ', a chci zaslat konkrétní nabídky, až budou k dispozici.' + "\n" + formData.get('message');
+					formData.set('message', dotazString);
 				}
-			}
+				const button = document.getElementById('registrationTour');
+				const spinner = document.querySelector('.spinner');
 
-		}
-		if (interestsString) {
-			var dotazString = 'Mám předběžný zájem o:' + interestsString + ', a chci zaslat konkrétní nabídky, až budou k dispozici.' + "\n" + formData.get('message');
-			formData.set('message', dotazString);
-		}
-		const button = document.getElementById('registrationTour');
-		const spinner = document.querySelector('.spinner');
-
-		button.disabled = true; // Disable the button to prevent multiple submissions
-		spinner.classList.remove('hidden'); // Show the spinner
-		button.firstElementChild.classList.add('hidden');
-		// Send the form data using fetch
-		fetch('/dotaz.php', {
-			method: 'POST',
-			body: formData // Send the FormData object directly
-		})
-			.then(response => response.text()) // or response.json() if expecting JSON response
-			.then(data => {
-				if (data == "OK") {
-					spinner.classList.add('hidden'); // Hide the spinner
-					document.getElementById('registrationForm').style.display = 'none';
-					document.getElementById('registrationSuccess').style.display = 'flex';
-					document.getElementById('registrationError').style.display = 'none';
-				}
-			})
-			.catch(error => {
-				console.error('Error:', error);
+				button.disabled = true; // Disable the button to prevent multiple submissions
+				spinner.classList.remove('hidden'); // Show the spinner
+				button.firstElementChild.classList.add('hidden');
+				// Send the form data using fetch
+				fetch('/dotaz.php', {
+					method: 'POST',
+					body: formData // Send the FormData object directly
+				})
+					.then(response => response.text()) // or response.json() if expecting JSON response
+					.then(data => {
+						if (data == "OK") {
+							spinner.classList.add('hidden'); // Hide the spinner
+							document.getElementById('registrationForm').style.display = 'none';
+							document.getElementById('registrationSuccess').style.display = 'flex';
+							document.getElementById('registrationError').style.display = 'none';
+						}
+					})
+					.catch(error => {
+						console.error('Error:', error);
+					});
 			});
+		});
 
-		//kontrola OK nebo ERR
+
 	});
 
 	// date selected from terminy
